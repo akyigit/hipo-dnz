fbddac11064e5a3807ce0ac7edb329ec0c556590
Deniz Akyigit <akyigitdeniz@gmail.com>
2022-04-19 02:41:21 +0300

    changed age and years_in_hipo to string & deleted company and team vars

---
+++ hipo-dnz/MemberData.swift
@@ -0,0 +1,21 @@
+//
+//  MemberData.swift
+//  hipo-dnz
+//
+//  Created by Deniz AKYİĞİT on 19.04.2022.
+//
+
+import Foundation
+
+struct MemberData: Codable {
+    
+    var id: Int
+ //   var company: String
+ //   var team: String
+    var name: String
+    var age: String
+    var location: String
+    var github: String
+    var position: String
+    var years_in_hipo: String
+}
